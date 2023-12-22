e5f7962e3fb7c2719092dabb24cb182f23cf4234
LaifiX - Peter Beňo <154002029+LaifiX@users.noreply.github.com>
2023-12-22 12:20:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+
+$('.fa-bars').click(function(){
+ $(this).toggleClass('fa-times');
+ $('.navbar').toggleClass('nav-toggle');
+
+});
+
+$(window).on('scroll load',function(){
+
+    $('.fa-bars').removeClass('fa-times');
+    $('.navbar').removeClass('nav-toggle');
+
+    if($(window).scrollTop() > 30) {
+        $('header').addClass('header-active');
+
+    }else {
+        $('header').removeClass('header-active');
+    }
+
+
+
+    
+    $('section').each(function(){
+       
+        let top = $(window).scrollTop();
+        let height = $(this).height();
+        let offset = $(this).offset().top - 200;
+        let id = $(this).attr('id');
+ 
+        if(top >= offset && top <offset + height) {
+          $('.navbar ul li a').removeClass('active');
+          $('.navbar').find(`[href="#${id}"]`).addClass('active');
+ 
+        }
+     });
+ 
+ });
+ 
+ })
+
+    /*  Tento zakomentovany mi nefungoval
+    
+    $('section').each(function(){
+        var top = $(window).scrollTop();
+        var id = $(this).attr('id');
+        var height = $(this).height();
+        var top = $(this). offset().top - 200;
+        if(top >= offset && top < height + offset){
+            $('.navbar ul li a').removeClass('active');
+            $('.navbar'). find('[href="#' + id + '"]').addClass('active');
+    }
+});
+
+});
+
+});
+
+*/
